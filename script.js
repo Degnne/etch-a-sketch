0304a4e24d9c0f55e37edeffae3e8181e36990a8
Jeff Bee <jeffreypbee@gmail.com>
2021-12-22 15:26:27 -0500

    Added additional functionality

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let brushColor = "#000000";
 const backgroundColor = "#ffffff";
-let canvasSize = 16;
+let canvasSize = 32;
+let isRainbow = false;
 const canvasContainerElement =  document.querySelector(".canvas-container");
 
 // Add event listeners to buttons
+const rainbowButton = document.querySelector("#rainbow");
+rainbowButton.addEventListener("click", () => toggleRainbow());
 const eraserButton = document.querySelector("#eraser");
 eraserButton.addEventListener("click", () => changeBrushColor(backgroundColor));
 const clearCanvasButton = document.querySelector("#clear-canvas");
@@ -21,7 +24,7 @@ function createCanvas() {
         for(let j = 0; j < canvasSize; j++) {
             const newPixel = document.createElement("div");
             newPixel.classList.add("pixel");
-            newPixel.addEventListener("mouseenter", () => {changePixelColor(newPixel, brushColor)});
+            newPixel.addEventListener("mouseenter", () => {changePixelColor(newPixel, getBrushColor())});
             newRow.appendChild(newPixel);
         }
         canvasElement.appendChild(newRow);
@@ -45,6 +48,40 @@ function changePixelColor(pixel, color) {
 
 function changeBrushColor(color) {
     brushColor = color;
+}
+
+function getBrushColor() {
+    if (isRainbow) {
+        return getRandomColor();
+    }
+    else {
+        return brushColor;
+    }
+}
+
+function getRandomColor() {
+    let color = "#";
+    for (let i = 1; i <= 6; i++) {
+        let randomNumber = getRandomInt(16);
+        switch(randomNumber) {
+            case 10: color += "a"; break;
+            case 11: color += "b"; break;
+            case 12: color += "c"; break;
+            case 13: color += "d"; break;
+            case 14: color += "e"; break;
+            case 15: color += "f"; break;
+            default: color += randomNumber;
+        }
+    }
+    return color;
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function toggleRainbow() {
+    isRainbow = !isRainbow;
 }
 
 createCanvas();
