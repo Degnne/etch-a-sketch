418007b71aa1a7d86f1ec386506717f4ac00aba7
Jeff Bee <jeffreypbee@gmail.com>
2021-12-22 14:14:39 -0500

    Added basic functionality

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-let drawColor = "#000000";
+let brushColor = "#000000";
+const backgroundColor = "#ffffff";
 let canvasSize = 16;
-const canvasElement =  document.getElementById("canvas");
+const canvasContainerElement =  document.querySelector(".canvas-container");
+
+// Add event listeners to buttons
+const eraserButton = document.querySelector("#eraser");
+eraserButton.addEventListener("click", () => changeBrushColor(backgroundColor));
+const clearCanvasButton = document.querySelector("#clear-canvas");
+clearCanvasButton.addEventListener("click", () => clearCanvas());
 
 function createCanvas() {
     deleteCanvas();
+    const canvasElement = document.createElement("div");
+    canvasElement.id = "canvas";
+    canvasContainerElement.appendChild(canvasElement);
     for(let i = 0; i < canvasSize; i++) {
+        //create a row of pixels
+        const newRow = document.createElement("div");
+        newRow.classList.add("canvas-row");
         for(let j = 0; j < canvasSize; j++) {
-            //const pixel = document.createElement("div");
-            //pixel.className = "pixel";
-            //canvasElement.appendChild(pixel);
+            const newPixel = document.createElement("div");
+            newPixel.classList.add("pixel");
+            newPixel.addEventListener("mouseenter", () => {changePixelColor(newPixel, brushColor)});
+            newRow.appendChild(newPixel);
         }
+        canvasElement.appendChild(newRow);
     }
 }
 
@@ -18,11 +33,18 @@ function deleteCanvas() {
 }
 
 function clearCanvas() {
-    //set all divs in canvas to white
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(pixel =>
+        changePixelColor(pixel, backgroundColor)
+    );
 }
 
-function changePixelColor() {
+function changePixelColor(pixel, color) {
+    pixel.style.backgroundColor = color;
+}
 
+function changeBrushColor(color) {
+    brushColor = color;
 }
 
 createCanvas();
